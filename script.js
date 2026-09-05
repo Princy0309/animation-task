ef52b9cb480051621c396ebe34cc27fd3374a9aa
Princy Mishra <princyyy03@gmail.com>
2026-09-05 09:05:18 +0530

    Add background with gradient and ground

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ canvas.height = 450;
 const c = canvas.getContext('2d');
 
 
-c.fillStyle = '#e94560';
-c.fillRect(100, 100, 100, 100);
+function drawBackgroundColor(){
 
+    const gradient = c.createLinearGradient(0, 0, 0, 350);
+
+    gradient.addColorStop(0, "darkblue");
+    gradient.addColorStop(1, "lightblue");
+
+    c.fillStyle = gradient;
+
+    c.fillRect(0, 0, canvas.width, 350);
+
+    c.fillStyle = "green";
+    c.fillRect(0, 350, canvas.width, 100);
+}
+
+drawBackgroundColor();
