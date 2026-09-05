c7eddcdd29c55ad2ff6ebca59f98bf4821022b43
Princy Mishra <princyyy03@gmail.com>
2026-09-05 08:46:29 +0530

    canvas resizing and Add 2d context

---
+++ script.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById('gameCanvas');
+
+canvas.width = 800;
+canvas.height = 450;
+
+const c = canvas.getContext('2d');
+
+
+c.fillStyle = '#e94560';
+c.fillRect(100, 100, 100, 100);
+
